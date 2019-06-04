8a36fdf463f7d24376eb42aa8a18a0f4d82adf3e
Stephen <smparkin@calpoly.edu>
2019-06-03 18:35:19 -0700

    initial

---
+++ Watch App Extension/HostingController.swift
@@ -0,0 +1,17 @@
+//
+//  HostingController.swift
+//  Watch App Extension
+//
+//  Created by Stephen Parkinson on 6/3/19.
+//  Copyright © 2019 Stephen Parkinson. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController : WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
---
+++ Watch App Extension/NotificationView.swift
@@ -0,0 +1,23 @@
+//
+//  NotificationView.swift
+//  Watch App Extension
+//
+//  Created by Stephen Parkinson on 6/3/19.
+//  Copyright © 2019 Stephen Parkinson. All rights reserved.
+//
+
+import SwiftUI
+
+struct NotificationView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct NotificationView_Previews : PreviewProvider {
+    static var previews: some View {
+        NotificationView()
+    }
+}
+#endif
